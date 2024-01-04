02c32c0761a5624dcc05c0a3c31bbc05aa684044
Nelly Porvatova <dolgikhhel@yandex.ru>
2024-01-04 21:59:00 +0400

    last3 commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,28 +1,25 @@
-    function chooseHole(max) {  
-        return Math.floor(Math.random() * max);              
-    }   
-    holes = document.getElementsByClassName('hole');         
-    const img = '<img src="./img/goblin.png">'; 
-    let currentHole = 0;
-    let newHole= 0;       
-    function runGame() {          
-        setInterval(() => {
-            newHole = chooseHole(holes.length - 1);                       
-            if (newHole !== currentHole){                
-                holes[newHole].innerHTML = img;              
-                holes[currentHole].innerHTML = ' ';  
-                currentHole = newHole;
-            } 
-            },                 
-        1000);      
-    }
-    runGame();
-    
-
-    
-    
-
-
-
-    
-    
+function chooseHole(max) {
+  return Math.floor(Math.random() * max);
+}
+const holes = document.getElementsByClassName('hole');
+const img = '<img src="./img/goblin.png">';
+let currentHole = 0;
+let newHole = 0;
+function runGame() {
+  setInterval(
+    () => {
+      newHole = chooseHole(holes.length - 1);
+      if (newHole === currentHole && newHole !== (holes.length-1)) {
+        newHole += 1;
+      }
+      if (newHole === currentHole && newHole === (holes.length-1)) {
+        newHole -= 1;
+      }
+      holes[newHole].innerHTML = img;
+      holes[currentHole].innerHTML = ' ';
+      currentHole = newHole;
+    },
+    1000,
+  );
+}
+runGame();
